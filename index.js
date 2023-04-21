dad44c49557ffbeb18847454396719a4d0518eb7
Jonas Stettner <mail@jstet.net>
2023-04-21 16:21:26 +0200

    initial

---
+++ index.js
@@ -0,0 +1,13 @@
+//TASK 4a
+//let the console tell that the script is loaded
+
+// TASK 4b
+// Select the polyline element on the page
+
+// Define an array of colors to loop through
+
+// Initialize a variable to keep track of the current color index
+
+// Create a loop (for example a for loop) that iterates through the color array using the index 
+  // set the new color to the selected line
+  // for ecah iteration enter 1 second delay to see the fox partying
