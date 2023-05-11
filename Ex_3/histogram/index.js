7b87020514a078cba5ab437dd56a48053042ac40
Jonas Stettner <mail@jstet.net>
2023-05-12 01:13:51 +0200

    solving ex3 w/o doc content

--- Ex_3/histogram/index.js
+++ Ex_3/histogram/index.js
@@ -17,9 +17,10 @@ const margin = {
   right: 20,
   bottom: 50,
 };
-const visWidth= width - margin.left - margin.right;
-const visHeight= height - margin.bottom;
-console.log(visHeight);
+const visWidth = width - margin.left - margin.right;
+const visHeight = height - margin.bottom;
+
+
 // We select the slider #bins-slider and add a input listener that adjusts
 // the value of the <span> #bins-counter to the currently selected value and calls the method update()
 // For more details, see https://github.com/d3/d3-selection#selection_on
@@ -34,65 +35,73 @@ d3.select("#bins-slider").on("input", (e) => {
 // The update function should create a bar chart in the <svg> element with the id #chart
 // Use the methods enter() to create new bars, exit() to remove no longer needed bars and merge() to update existing bars
 function update(numbins) {
+
   // TASK 2.1, create an linear x scale given the extent of the data to the width of the svg. Be careful to consider the left and right margins.
   const min_x = d3.min(data, d => d.rate)
   const max_x = d3.max(data, d => d.rate)
   const xScale = d3.scaleLinear()
-  .domain([min_x, max_x])
-  .range([0, visWidth]);
+    .domain([min_x, max_x])
+    .range([0, visWidth]).nice();
 
 
   // TASK 2.2: Bin the data using https://github.com/d3/d3-array/blob/main/README.md#bin (Examples: https://observablehq.com/@d3/d3-bin )
   // Make sure to specify the following attributes: domain and threshold, threshold is the approximate number of bins.
   // Afterwards bin the input data
-  const bin_function = d3.bin().domain(xScale.domain()).thresholds(numbins)
-  const bins = bin_function(data, d => d.rate);
+  const bin_function = d3.bin().domain(xScale.domain()).thresholds(numbins).value(d => d.rate)
+  const bins = bin_function(data);
 
   // Task 2.3 Create a linear y scale to given the number of elements in the bins [0, max(elements in bin)]
-  const yScale = d3.scaleLinear().domain([0, d3.max(bins, function(d) { return d.length })]).range([0,visHeight]);
- 
+  const yScale = d3.scaleLinear().domain([0, d3.max(bins, function (d) { return d.length })]).range([visHeight, 0]);
+
   // TASK 2.4. Select the #chart <svg> element.
   const svg = d3.select("#chart")
+  svg.selectAll("g").remove();
+
+  const viz = svg.append("g").attr("transform", "translate(" + margin.left + ")")
 
   // TASK 2.5 Remove old rectangles that are no longer needed using either selection.remove() or exit()
-  d3.selectAll("rect").remove();
 
   // TASK 2.6. Connect all <rect> children (using data and enter) of the svg with the bins (be careful to use the length of the bins, not the bin elements)
-  const rects = svg.append("g").selectAll("rect").data(bins, function(d) { return d.length }).enter();
+  const rects = viz.selectAll("rect").data(bins, function (d) { return d.length }).enter();
 
   // Task 3 Add a tooltip to the visualization
   // Hint:
   // 1. Add an empty <div> element with id "tooltip" to the body of the document and make it invisible
-  //const tooltip = d3.select("body").append("div").attr("id", "tooltip").style("display","hidden");
-  
+  const tooltip = d3.select("body").append("div").attr("id", "tooltip").style("visibility", "hidden").style("margin-top","200px");
+
   // 3.2. Add a mouseover event listener to the <rect> elements and make the <div> element visible and set the text of the <div> element to the data of the hovered <rect> element
   // Rember that the data is accessible via the second parameter of the event listener function
   // 3.3. On mousemove, set the text of the <div> element to the data of the hovered <rect> element
   // 3.4. On mouseout, hide the <div> element
   // 3.5. Position the <div> element using absolute positioning and the pageX and pageY properties of the mousemove event
-  // rects.
-
-
-
   // Task 2.7 Append a rectangle for every newly added rect to the visualization, update their position (x, y, width, height)
   // Do not forget to add the tooltip event listeners to the new rectangles (see Task 3)
   //Define the bar width 
-  let bar_width=visWidth/numbins;
+
   rects.append("rect")
-    .attr("x", 1)
-    .attr("transform", function(d) { return "translate(" + xScale(d.x0) + "," + (visHeight - yScale(d.length))+ ")"; })
-    //.attr("transform", function(d) { return "translate(" + xScale(d.x0) + "," + (visHeight - yScale(d.length))+ ")"; })
-    .attr("width", function(d) { return xScale(d.x1) - xScale(d.x0) -1 ; })
-    .attr("height", function(d) { return yScale(d.length); });
-   // ...
+    .attr("x", function (d) { return xScale(d.x0); })
+    .attr("transform", function (d) { return `translate(0, ${yScale(d.length)})`; })
+    .attr("width", function (d) { return xScale(d.x1) - xScale(d.x0); })
+    .attr("height", function (d) { return visHeight - yScale(d.length); })
+    .on("mouseover", (event,d) => {tooltip.style("visibility", "visible").html(`Number of Counties: ${d.length}`) })
+    .on("mousemove", (event,d) => {tooltip.style("visibility", "visible").html(`Number of Counties: ${d.length}`) })
+    .on("mouseout", () => {tooltip.style("visibility", "hidden")});
+  // ...
 
   // Task 2.8 add an x axis to the visualization
   // Hint: https://github.com/d3/d3-axis
-  //...
+  // Add X Axis for years
+  // Add scales to axis
+  var xAxis = d3.axisBottom().scale(xScale);
+
+  viz.append("g").call(xAxis).attr("transform", "translate(0," + visHeight + ")")
 
   // Task 2.9 add a y axis to the visualization
-  //...  
+  var yAxis = d3.axisLeft().scale(yScale);
+
+  viz.append("g").call(yAxis)
+  console.log(data)
 }
 
 // call update function with initial value to create visualization on first site visit
-//update(20);
+update(20);
