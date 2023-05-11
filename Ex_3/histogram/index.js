a5f3eb447eb4e24d4142d060238f4bfd64572125
Jonas Stettner <mail@jstet.net>
2023-05-11 14:52:41 +0200

    partly solving ex3

--- Ex_3/histogram/index.js
+++ Ex_3/histogram/index.js
@@ -33,40 +33,48 @@ d3.select("#bins-slider").on("input", (e) => {
 // Use the methods enter() to create new bars, exit() to remove no longer needed bars and merge() to update existing bars
 function update(numbins) {
   // TASK 2.1, create an linear x scale given the extent of the data to the width of the svg. Be careful to consider the left and right margins.
-  const xScale = ...
+  const min_x = d3.min(data, d => d.rate)
+  const max_x = d3.max(data, d => d.rate)
+  const xScale = d3.scaleLinear()
+  .domain([min_x, max_x])
+  .range([0, visWidth]);
+
 
   // TASK 2.2: Bin the data using https://github.com/d3/d3-array/blob/main/README.md#bin (Examples: https://observablehq.com/@d3/d3-bin )
   // Make sure to specify the following attributes: domain and threshold, threshold is the approximate number of bins.
   // Afterwards bin the input data
-  const bin_function = ...
-  const bins = bin_function(data);
+  const bin_function = d3.bin().domain(xScale.domain).thresholds(xScale.ticks(numbins))
+  const bins = bin_function(data, d => d.rate);
 
   // Task 2.3 Create a linear y scale to given the number of elements in the bins [0, max(elements in bin)]
-  const yScale = ...
-
+  const yScale = d3.scaleLinear().domain([0, d3.max(bins, function(d) { return d.length })]).range([visHeight,0]);
+ 
   // TASK 2.4. Select the #chart <svg> element.
-  const svg = ...;
+  const svg = d3.select("#chart")
 
   // TASK 2.5 Remove old rectangles that are no longer needed using either selection.remove() or exit()
-  ...
+  d3.selectAll("rect").remove();
 
   // TASK 2.6. Connect all <rect> children (using data and enter) of the svg with the bins (be careful to use the length of the bins, not the bin elements)
-  const rects = ...;
+  const rects = svg.append("g").selectAll("rect").data(bins, function(d) { return d.length }).enter();
 
   // Task 3 Add a tooltip to the visualization
   // Hint:
   // 1. Add an empty <div> element with id "tooltip" to the body of the document and make it invisible
-  const tooltip = d3.select("body")...
+  const tooltip = d3.select("body").append("div").attr("id", "tooltip").style("display","hidden")
   
   // 3.2. Add a mouseover event listener to the <rect> elements and make the <div> element visible and set the text of the <div> element to the data of the hovered <rect> element
   // Rember that the data is accessible via the second parameter of the event listener function
   // 3.3. On mousemove, set the text of the <div> element to the data of the hovered <rect> element
   // 3.4. On mouseout, hide the <div> element
   // 3.5. Position the <div> element using absolute positioning and the pageX and pageY properties of the mousemove event
+  // rects.
+
+
 
   // Task 2.7 Append a rectangle for every newly added rect to the visualization, update their position (x, y, width, height)
   // Do not forget to add the tooltip event listeners to the new rectangles (see Task 3)
-  rects.
+  rects.append("rect").attr("")
     ...
 
   // Task 2.8 add an x axis to the visualization
